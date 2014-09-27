7680d194a8ab643840ca9cd85e56d96f15e6bf7e
saintcross <miao0803@gmail.com>
2014-09-27 20:57:15 +0800

    Initial Commit

---
+++ n-puzzle/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  n-puzzle
+//
+//  Created by Jingwei MIAO on 9/27/14.
+//  Copyright (c) 2014 Jingwei MIAO. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
